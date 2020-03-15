633cd6a2f57e869df4e86e5001ac70d689f15b6a
Maxim Zuev <max-zu99@mail.ru>
2020-03-15 19:52:08 +0300

    Add random level generator

---
+++ Roguelike/Labyrinth.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections.Generic;
+
+namespace Roguelike
+{
+    internal class Labyrinth
+    {
+        private readonly int height;
+        private readonly int width;
+        private readonly int n;
+        private readonly bool[,] matrix;
+        private static readonly int[] Dx = {0, 0, -1, 1};
+        private static readonly int[] Dy = {-1, 1, 0, 0};
+
+        public Labyrinth(int height, int width)
+        {
+            this.height = height;
+            this.width = width;
+            n = width * height;
+            matrix = new bool[n, n];
+            for (var i = 0; i < n; ++i)
+            {
+                for (var j = 0; j < n; ++j)
+                {
+                    matrix[i, j] = false;
+                }
+            }
+
+            Build();
+        }
+
+        private void Build()
+        {
+            var used = new bool[n];
+            for (var i = 0; i < n; ++i)
+            {
+                used[i] = false;
+            }
+
+            var usedNumber = n;
+            var random = new Random();
+            var x = random.Next(width);
+            var y = random.Next(height);
+            var v = Id(y, x);
+            used[v] = true;
+            usedNumber--;
+            var neighbors = new List<int>();
+            var stack = new List<int>();
+            while (usedNumber > 0)
+            {
+                neighbors.Clear();
+                for (var i = 0; i < 4; ++i)
+                {
+                    var nx = x + Dx[i];
+                    var ny = y + Dy[i];
+                    if (!IsValidCell(ny, nx)) continue;
+                    var u = Id(ny, nx);
+                    if (!used[u])
+                    {
+                        neighbors.Add(u);
+                    }
+                }
+
+                if (neighbors.Count == 0)
+                {
+                    v = stack[stack.Count - 1];
+                    stack.RemoveAt(stack.Count - 1);
+                }
+                else
+                {
+                    stack.Add(v);
+                    var u = neighbors[random.Next(neighbors.Count)];
+                    used[u] = true;
+                    --usedNumber;
+                    matrix[u, v] = matrix[v, u] = true;
+                    v = u;
+                }
+
+                x = v % width;
+                y = v / width;
+            }
+        }
+
+        /**
+         * Checks is cells (y1, x1) and (y2, x2) are connected.
+         * Returns false if any coordinate is out of bounds.
+         */
+        public bool AreConnected(int y1, int x1, int y2, int x2)
+        {
+            if (!(IsValidCell(y1, x1) && IsValidCell(y2, x2)))
+            {
+                return false;
+            }
+
+            var v = Id(y1, x1);
+            var u = Id(y2, x2);
+            return matrix[v, u];
+        }
+
+        private bool IsValidY(int y)
+        {
+            return 0 <= y && y < height;
+        }
+
+        private bool IsValidX(int x)
+        {
+            return 0 <= x && x < width;
+        }
+
+        public bool IsValidCell(int y, int x)
+        {
+            return IsValidX(x) && IsValidY(y);
+        }
+
+        private int Id(int y, int x)
+        {
+            return y * width + x;
+        }
+    }
+}
--- Roguelike/RandomLevelFactory.cs
+++ Roguelike/RandomLevelFactory.cs
@@ -1,15 +1,129 @@
+using System;
+
 namespace Roguelike
 {
     public class RandomLevelFactory : ILevelFactory
     {
-        private const int DefaultHeight = 50;
-        private const int DefaultWidth = 50;
-        
+        private const int DefaultHeight = 15;
+        private const int DefaultWidth = 15;
+        private readonly Random random = new Random();
+        private const float WallProbability = 0.5f;
+
+        private static readonly int Height = RoundToOdd(DefaultHeight);
+        private static readonly int Width = RoundToOdd(DefaultWidth);
+        private static readonly int CellHeight = (Height - 1) / 2;
+        private static readonly int CellWidth = (Width - 1) / 2;
+        private readonly Labyrinth labyrinth = new Labyrinth(CellHeight, CellWidth);
+
+        private static int RoundToOdd(int x)
+        {
+            if (x % 2 == 0)
+            {
+                return x + 1;
+            }
+            else
+            {
+                return x;
+            }
+        }
+
         public Level CreateLevel()
         {
-            //TODO
-            //var board = new GameObject[DefaultHeight, DefaultWidth];
-            return null;
+            var boardTable = new GameObject[Height, Width];
+            CreateBorders(Height, Width, boardTable);
+            var dx = new[] {0, 1};
+            var dy = new[] {1, 0};
+            for (var cellRow = 0; cellRow < CellHeight; cellRow++)
+            {
+                for (var cellCol = 0; cellCol < CellWidth; cellCol++)
+                {
+                    var row = CellIdToBoard(cellRow);
+                    var col = CellIdToBoard(cellCol);
+                    AddEmptyCell(row, col, boardTable);
+                    for (var i = 0; i < 2; ++i)
+                    {
+                        if (!labyrinth.IsValidCell(cellRow + dy[i], cellCol + dx[i])) continue;
+                        ConnectCells(cellRow, cellCol, cellRow + dy[i], cellCol + dx[i], boardTable);
+                    }
+
+                    if (labyrinth.IsValidCell(cellRow + 1, cellCol + 1))
+                    {
+                        AddWallWithProbability(row + 1, col + 1, boardTable);
+                    }
+                }
+            }
+
+            AddPlayerCell(1, 1, boardTable);
+
+            var board = new Board(Width, Height, boardTable);
+            return new Level(board);
+        }
+
+        private void ConnectCells(int cellY1, int cellX1, int cellY2, int cellX2, GameObject[,] boardTable)
+        {
+            var row = CellIdToBoard(cellY1) + cellY2 - cellY1;
+            var col = CellIdToBoard(cellX1) + cellX2 - cellX1;
+            if (labyrinth.AreConnected(cellY1, cellX1, cellY2, cellX2))
+            {
+                AddEmptyCell(row, col, boardTable);
+            }
+            else
+            {
+                AddWallWithProbability(row, col, boardTable);
+            }
+        }
+
+        private static int CellIdToBoard(int id)
+        {
+            return 1 + id * 2;
+        }
+
+        private static void CreateBorders(int height, int width, GameObject[,] boardTable)
+        {
+            var rows = new[] {0, height - 1};
+            foreach (var row in rows)
+            {
+                for (var col = 0; col < width; ++col)
+                {
+                    AddWall(row, col, boardTable);
+                }
+            }
+
+            var cols = new[] {0, width - 1};
+            foreach (var col in cols)
+            {
+                for (var row = 0; row < height; ++row)
+                {
+                    AddWall(row, col, boardTable);
+                }
+            }
+        }
+
+        private static void AddWall(int row, int col, GameObject[,] boardTable)
+        {
+            boardTable[row, col] = new Wall(new Position(row, col));
+        }
+
+        private void AddWallWithProbability(int row, int col, GameObject[,] boardTable)
+        {
+            if (random.NextDouble() < WallProbability)
+            {
+                AddWall(row, col, boardTable);
+            }
+            else
+            {
+                AddEmptyCell(row, col, boardTable);
+            }
+        }
+
+        private static void AddEmptyCell(int row, int col, GameObject[,] boardTable)
+        {
+            boardTable[row, col] = new EmptyCell(new Position(row, col));
+        }
+
+        private static void AddPlayerCell(int row, int col, GameObject[,] boardTable)
+        {
+            boardTable[row, col] = new Player(new Position(row, col));
         }
     }
 }
