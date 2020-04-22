98878381cf81bfd8a93bb5dcf8c51db6ad5baf61
Maria Tigina <tiginamaria@gmail.com>
2020-04-22 12:47:05 +0300

    Fix Travis

--- RoguelikeTest/Properties/AssemblyInfo.cs
+++ RoguelikeTest/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2249E592-EAD6-4EBC-A190-FAF1B4601C6E")]
+[assembly: Guid("8E38992C-CB4E-46EC-8984-CC8FB610005F")]
 
 // Version information for an assembly consists of the following four values:
 //
