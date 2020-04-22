4463b3abc480bb3f4d46d45d27ca88c0400ca64e
Maria Tigina <tiginamaria@gmail.com>
2020-04-22 11:49:34 +0300

    Fix travis

--- RoguelikeTest/LevelConfigurationTests.cs
+++ RoguelikeTest/LevelConfigurationTests.cs
@@ -1,7 +1,9 @@
-﻿﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
 using Roguelike.Initialization;
 using Roguelike.Model;
- using Roguelike.Model.Mobs;
+using Roguelike.Model.Mobs;
 
  namespace TestRoguelike
 {
@@ -85,6 +87,68 @@ using Roguelike.Model;
             {
                 Assert.IsFalse(board.CheckOnBoard(new Position(-1, j)));
                 Assert.IsFalse(board.CheckOnBoard(new Position(height, j)));
+            }
+        }
+
+        private HashSet<Tuple<int, int>> GetConnectedCells(int height, int width, Labyrinth labyrinth, Tuple<int, int> cell)
+        {
+            var connectedCells = new HashSet<Tuple<int, int>>();
+            for (var i = 0; i < height; i++)
+            {
+                for (var j = 0; j < width; j++)
+                {
+                    if (labyrinth.AreConnected(i, j, cell.Item1, cell.Item2))
+                    {
+                        connectedCells.Add(new Tuple<int, int>(i, j));
+                    }
+                }
+            }
+
+            return connectedCells;
+        }
+        
+        [Test]
+        public void LabyrinthValidCellTest()
+        {
+            const int height = 3;
+            const int width = 6;
+            var labyrinth = new Labyrinth(height, width);
+            for (var i = 0; i < height; i++)
+            {
+                for (var j = 0; j < width; j++)
+                {
+                    Assert.IsTrue(labyrinth.IsValidCell(i, j));
+                }
+            }
+            for (var i = 0; i < height; i++)
+            {
+                Assert.IsFalse(labyrinth.IsValidCell(i, -1));
+                Assert.IsFalse(labyrinth.IsValidCell(i, width));
+            }
+            for (var j = 0; j < width; j++)
+            {
+                Assert.IsFalse(labyrinth.IsValidCell(-1, j));
+                Assert.IsFalse(labyrinth.IsValidCell(height, j));
+            }
+        }
+        
+        [Test]
+        public void TransConnectivityTest()
+        {
+            const int height = 3;
+            const int width = 4;
+            var labyrinth = new Labyrinth(height, width);
+            for (var i = 0; i < height; i++)
+            {
+                for (var j = 0; j < width; j++)
+                {
+                    var connectedCells = GetConnectedCells(height, width, labyrinth, new Tuple<int, int>(i, j));
+                    foreach (var connectedCell in connectedCells)
+                    {
+                        Assert.IsTrue(labyrinth.AreConnected(i, j, connectedCell.Item1, connectedCell.Item2));
+                        var transConnectedCells = GetConnectedCells(height, width, labyrinth, new Tuple<int, int>(i, j));
+                    }
+                }
             }
         }
     }
