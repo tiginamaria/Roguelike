d239ad834c2078ac20169bf80822072ebb923f6f
Maria Tigina <tiginamaria@gmail.com>
2020-04-29 01:55:05 +0300

    Fix code style

--- Roguelike/Model/PlayerModel/AbstractPlayer.cs
+++ Roguelike/Model/PlayerModel/AbstractPlayer.cs
@@ -7,17 +7,14 @@ namespace Roguelike.Model.PlayerModel
     public abstract class AbstractPlayer : Character
     {
         public EventHandler OnDie;
-
-        public abstract void PutOff(string inventoryType);
-
-        public abstract void PutOn(string inventoryType);
         
         protected AbstractPlayer(Position startPosition) : base(startPosition)
         {
         }
 
+        public abstract void PutOff(string inventoryType);
+        public abstract void PutOn(string inventoryType);
         public abstract List<InventoryItem> GetInventory();
-
         public abstract List<InventoryItem> GetAppliedInventory();
     }
 }
